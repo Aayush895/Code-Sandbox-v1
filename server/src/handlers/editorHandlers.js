366c1a8450f122bba8135287c8eabf2f58b1fcc6
Aayush895 <aayush895@gmail.com>
2026-03-08 22:35:24 +0530

    Added the write file handler for the server

--- server/src/handlers/editorHandlers.js
+++ server/src/handlers/editorHandlers.js
@@ -1,10 +1,9 @@
 import fs from 'node:fs/promises';
 
 function editorHandlers(socket) {
-  // Make the users join the room when they open the project
   // Room Id: projectId + file
 
-  // Event function / handler for reading the contents of a file when the file is double-cicked
+  // Event function / handler for joining a room when a file is selected
   async function joinRoom({ roomId }) {
     try {
       socket.join(roomId);
@@ -17,6 +16,7 @@ function editorHandlers(socket) {
     }
   }
 
+  // Event function / handler for reading the contents of a file
   async function readFileHandler({ filePath }) {
     try {
       const readData = await fs.readFile(filePath);
@@ -32,8 +32,26 @@ function editorHandlers(socket) {
     }
   }
 
+  // Event function / handler for writing the contents of a file
+  async function writeFileHandler({ filePath, roomId, fileData }) {
+    try {
+      const writeFileResponse = await fs.writeFile(filePath, fileData);
+      socket.to(roomId).emit('write-file-success', {
+        fileData: writeFileResponse,
+        activeFile: filePath,
+        message: 'File written successfully',
+      });
+    } catch (error) {
+      console.log('Error in writing the file: ', error);
+      socket.emit('Error', {
+        data: 'Error in writing the file',
+      });
+    }
+  }
+
   socket.on('join-room', joinRoom);
   socket.on('read-file', readFileHandler);
+  socket.on('write-file', writeFileHandler);
 }
 
 export default editorHandlers;
